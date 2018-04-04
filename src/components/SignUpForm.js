943acbdeb5182a177380662f3a5414acb0cceec8
byio <ben@benyang.io>
2018-04-04 13:33:34 +0800

    use a chain of axios promises to create user on sign up, and send otp to new user

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { FormLabel, FormInput, Button } from 'react-native-elements';
 
+const ROOT_URL = "https://us-central1-rn-onetime-pw.cloudfunctions.net";
+
 class SignUpForm extends Component {
   // es2017 syntax for declaring state outside of constructor function
     /*
@@ -14,7 +16,13 @@ class SignUpForm extends Component {
 
   // helper methods (use arrow functions so it's not necessary bind the handler to 'this' context; es2017 syntax)
   handleSignup = () => {
-
+    // create user
+    axios.post(`${ROOT_URL}/createUser`, { phone: this.state.phone })
+         .then(() => {
+           // request otp
+           axios.post(`${ROOT_URL}/requestOtp`, { phone: this.state.phone })
+                .catch(err => res.send({ error: err }));
+         })
   }
 
   // render method
